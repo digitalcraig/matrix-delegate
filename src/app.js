e513b0d29d8f9626a33f279f7c374beabf2f6001
Craig Leikis <craig@leikis.org>
2023-11-04 04:47:22 -0500

    Add support for msc3575 syncv3 proxy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ var app = http.createServer(function(req,res){
 
     var port = process.env.MATRIX_PORT || 8443
     var mserver = process.env.MATRIX_SERVER;
-    
+    var syncserver = process.env.SYNC_SERVER || null;
+    var syncport = process.env.SYNC_PORT || 8443;
+
     if (!mserver) {
         mserver = "matrix."+req.headers.host.split(":")[0];
     }
@@ -13,6 +15,13 @@ var app = http.createServer(function(req,res){
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ "m.server" : mserver+":"+port }, null, 3));
+    if (syncserver != null) {
+        res.end(JSON.stringify({"m.server" : mserver+":"+port,
+                                "m.homeserver" : mserver+":"+port,   
+                                "org.matrix.msc3575.proxy": syncserver+":"+syncport}, null, 3));
+    } else {
+        res.end(JSON.stringify({"m.server" : mserver+":"+port, 
+                                "m.homeserver" : mserver+":"+port,}, null, 3));
+    }
 });
 app.listen(3000);
